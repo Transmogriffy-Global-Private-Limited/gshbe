f454d2875bb40b8837cb9b158f5b86b72775d032
AnubhabDey-TransEV <anubhabdey@tgplin.com>
2025-12-11 16:29:18 +0530

    Changed the DB config to read and use creds from envvars

--- piccolo_conf.py
+++ piccolo_conf.py
@@ -1,14 +1,19 @@
 # db/piccolo_conf.py
+import os
+from dotenv import load_dotenv
 from piccolo.conf.apps import AppRegistry
 from piccolo.engine.postgres import PostgresEngine
 
+# Load .env from project root
+load_dotenv()
+
 DB = PostgresEngine(
     config={
-        "database": "gshbedb",
-        "user": "postgres",
-        "password": "Tgpldev2025",
-        "host": "localhost",
-        "port": 5432,
+        "database": os.getenv("DBNAME"),
+        "user": os.getenv("DBUSER"),
+        "password": os.getenv("DBPASS"),
+        "host": os.getenv("DBHOST"),
+        "port": int(os.getenv("DBPORT", "5432")),
     }
 )
 
