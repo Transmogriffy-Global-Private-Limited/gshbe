7b59b7c6a69a7a2aba39d1e6359cc93c2c036309
AnubhabDey-TransEV <anubhabdey@tgplin.com>
2025-12-22 12:47:26 +0530

    Added basic CORS

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # app/main.py
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from piccolo.engine import engine_finder
 
 from auth.endpoints.router import router as auth_router
@@ -24,6 +25,14 @@ app = FastAPI(
     version="0.1.0",
     lifespan=lifespan,
     docs_url="/swagger",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],          # allow all origins
+    allow_credentials=False,      # MUST be False when origins=["*"]
+    allow_methods=["*"],          # allow all HTTP methods
+    allow_headers=["*"],          # allow all headers
 )
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
