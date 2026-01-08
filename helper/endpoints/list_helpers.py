3acc9dfbf643cfe04067e35fe4ea85de2638f1c2
abhranil200305 <transmogrify40@outlook.com>
2026-01-08 10:50:11 +0530

    Changes before updating venv

--- helper/endpoints/list_helpers.py
+++ helper/endpoints/list_helpers.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 from helper.logic.list_helpers import list_helpers
+from helper.structs.list_helpers import HelperListResponse
 
 router = APIRouter(prefix="/helpers", tags=["Helpers"])
 
 
-@router.get("/")
+@router.get("/", response_model=HelperListResponse)
 async def get_helpers():
     return await list_helpers()
