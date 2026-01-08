a3f114e5ba972fd7925bce594acde7563aac8b49
abhranil200305 <transmogrify40@outlook.com>
2026-01-08 14:29:35 +0530

    Changes of list helper

--- helper/endpoints/list_helpers.py
+++ helper/endpoints/list_helpers.py
@@ -1,12 +1,8 @@
 from fastapi import APIRouter
 from helper.logic.list_helpers import list_helpers
-from helper.structs.list_helpers import HelperListResponse
 
-router = APIRouter(
-    prefix="/helper",
-    tags=["Helper"]
-)
+router = APIRouter()
 
-@router.get("/list_helpers", response_model=HelperListResponse)
+@router.get("/helper/list_helpers")
 async def get_helpers():
     return await list_helpers()
