8678d28b1df36f13146189faa84b5043ec75cc65
abhranil200305 <transmogrify40@outlook.com>
2026-01-09 17:19:59 +0530

    for admin

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# app/main.py
-
 from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from piccolo.engine import engine_finder
@@ -11,7 +8,6 @@ from auth.endpoints.router import router as auth_router
 from profiles.endpoints.router import router as profiles_router
 from helper.endpoints.router import router as helper_router
 from admin.endpoints.router import router as admin_router
-
 
 load_dotenv()
 
@@ -41,8 +37,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Routers
-app.include_router(auth_router, prefix="/auth", tags=["auth"])
-app.include_router(profiles_router, prefix="/profiles", tags=["profiles"])
-app.include_router(helper_router, prefix="/helper", tags=["helper"])
-app.include_router(admin_router, prefix="/admin", tags=["admin"])
+app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+app.include_router(profiles_router, prefix="/profiles", tags=["Profiles"])
+app.include_router(helper_router, prefix="/helper", tags=["Helper"])
+app.include_router(admin_router)   # ✅ ONLY THIS
