4a0ad59cc9b8391d0b473d5d4c9733a769b038f4
abhranil200305 <transmogrify40@outlook.com>
2026-01-09 10:07:33 +0530

    for swegger

---
+++ helper/tables/registration.py
@@ -0,0 +1,9 @@
+from piccolo.table import Table
+from piccolo.columns import UUID, Varchar, Integer
+
+
+class Registration(Table):
+    registration_id = UUID(primary_key=True)
+    role = Varchar()
+    capacity = Varchar()
+    profile_kind = Varchar()
