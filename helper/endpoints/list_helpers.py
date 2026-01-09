67cf22a2c70f1ed137d537c902dd5b4a2eb304b8
abhranil200305 <transmogrify40@outlook.com>
2026-01-09 10:44:33 +0530

    for swegger

--- helper/endpoints/list_helpers.py
+++ helper/endpoints/list_helpers.py
@@ -1,16 +1,9 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter
 from helper.logic.list_helpers import list_helpers_service
 from helper.structs.dtos import HelperListOut
 
 router = APIRouter()
 
-
-@router.get(
-    "/list_helpers",
-    response_model=HelperListOut,
-    status_code=status.HTTP_200_OK,
-    summary="List available helpers",
-)
+@router.get("/list_helpers", response_model=HelperListOut)
 async def list_helpers_endpoint():
-    helpers = await list_helpers_service()
-    return {"items": helpers}
+    return await list_helpers_service()
