c8a635a9a2797239dd36538dd6a618d436752441
abhranil200305 <transmogrify40@outlook.com>
2026-01-09 10:45:59 +0530

    for swegger

---
+++ helper/tables/_init_.py
@@ -0,0 +1,9 @@
+from .registration import Registration
+from .helper_personal_profile import HelperPersonalProfile
+from .helper_institutional_profile import HelperInstitutionalProfile
+
+__all__ = [
+    "Registration",
+    "HelperPersonalProfile",
+    "HelperInstitutionalProfile",
+]
