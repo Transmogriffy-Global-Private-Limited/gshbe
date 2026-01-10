e2dfe98caee597db72026369dd182a111179cce8
abhranil200305 <transmogrify40@outlook.com>
2026-01-10 10:30:43 +0530

    for admin

--- admin/piccolo_app.py
+++ admin/piccolo_app.py
@@ -1,5 +1,10 @@
+# admin/piccolo_app.py
 from piccolo.conf.apps import AppConfig
 
-class AdminAppConfig(AppConfig):
-    app_name = "admin"
-    migrations_folder_path = "admin/piccolo_migrations"
+APP_CONFIG = AppConfig(
+    app_name="admin",
+    migrations_folder_path="admin/piccolo_migrations",
+    table_classes=[
+        "admin.tables.admin.Admin",
+    ],
+)
